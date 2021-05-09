1e03aa21113d7dde2de16e2c27c960218b7efa6b
illias <khomko.illia@gmail.com>
2021-05-10 02:13:25 +0900

    reconfigure lcoales

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require('path')
+
 module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "fr"],
+    localePath: path.resolve('./public/static/locales')
   },
   react: {
     useSuspense: false,
