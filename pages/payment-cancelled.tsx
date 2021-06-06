d43d1f4a0274bbe1ff18e4a860da021d3270dc52
illias <khomko.illia@gmail.com>
2021-06-06 12:40:20 +0900

    remove unused reservation constant for local storage

--- pages/payment-cancelled.tsx
+++ pages/payment-cancelled.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import { PageWrapper } from "../components/atoms/Layout";
-import { LOCAL_STORAGE_RESERVATION } from "../constants";
 
 type IPaymentCancelledProps = {};
 
 const PaymentCancelled: React.FC<IPaymentCancelledProps> = ({ ...props }) => {
-  React.useEffect(() => {
-    localStorage.removeItem(LOCAL_STORAGE_RESERVATION);
-  }, []);
   return <PageWrapper>Unfortunate your payment has been cancelled</PageWrapper>;
 };
 
--- pages/payment-success/[[...session]].tsx
+++ pages/payment-success/[[...session]].tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import { PageWrapper } from "../../components/atoms/Layout";
-import { LOCAL_STORAGE_RESERVATION } from "../../constants";
 
 type IPaymentSuccessProps = {};
 
 const PaymentSuccess: React.FC<IPaymentSuccessProps> = ({ ...props }) => {
-  React.useEffect(() => {
-    localStorage.removeItem(LOCAL_STORAGE_RESERVATION);
-  }, []);
   return <PageWrapper>Thank you for your payment!</PageWrapper>;
 };
 
