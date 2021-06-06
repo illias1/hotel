536f1bb8cddc62a739557fc51813d92ce96a382e
illias <khomko.illia@gmail.com>
2021-06-06 22:42:10 +0900

    add payment result pages ui

--- pages/payment-cancelled.tsx
+++ pages/payment-cancelled.tsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { Result } from "antd";
+import Link from "next/link";
+
 import { PageWrapper } from "../components/atoms/Layout";
 
 type IPaymentCancelledProps = {};
 
 const PaymentCancelled: React.FC<IPaymentCancelledProps> = ({ ...props }) => {
-  return <PageWrapper>Unfortunate your payment has been cancelled</PageWrapper>;
+  return (
+    <PageWrapper>
+      <Result
+        status="error"
+        title="Unfortunately, your payment failed"
+        subTitle="Please check your card has enough funds and you submitted the correct information."
+        extra={[
+          <Link href="/">
+            <a>Home</a>
+          </Link>,
+          <Link href="/">
+            <a>Back to checkout</a>
+          </Link>,
+        ]}
+      ></Result>
+    </PageWrapper>
+  );
 };
 
 export default PaymentCancelled;
--- pages/payment-success/[[...session]].tsx
+++ pages/payment-success/[[...session]].tsx
@@ -1,10 +1,30 @@
 import React from "react";
+import Link from "next/link";
+import { Result } from "antd";
+
 import { PageWrapper } from "../../components/atoms/Layout";
 
 type IPaymentSuccessProps = {};
 
 const PaymentSuccess: React.FC<IPaymentSuccessProps> = ({ ...props }) => {
-  return <PageWrapper>Thank you for your payment!</PageWrapper>;
+  return (
+    <PageWrapper>
+      <Result
+        status="success"
+        title="Thank you for your payment!"
+        subTitle="You will shortly receive a confirmation email. You can also always check the booking on your profile page :)"
+        extra={[
+          <Link href="/">
+            <a>Home</a>
+          </Link>,
+          <Link href="/profile">
+            <a>Profile page</a>
+          </Link>,
+        ]}
+      />
+      ,
+    </PageWrapper>
+  );
 };
 
 export default PaymentSuccess;
